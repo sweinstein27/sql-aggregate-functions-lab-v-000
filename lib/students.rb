b76165e229313a7c506fea91b97180c4e337c9fc
Scott Weinstein <sweinstein27@gmail.com>
2018-06-25 17:24:18 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -18,8 +18,11 @@ end
 def total_tardies_for_all_students
   "SELECT sum(tardies)
 FROM students;"
-  
+
 end
 
 def average_gpa_for_9th_grade
+  "SELECT MAX(gpa)
+FROM students
+  WHERE grade = 9 ;"
 end
